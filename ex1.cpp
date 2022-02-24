68a48b9259b673caec4635e4b2856d6cda3e3654
james10911178 <100347704+james10911178@users.noreply.github.com>
2022-02-24 18:47:04 +0800

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    printf("I am %d years old \n",16);
+    
+    printf("Wy weight is %dkg \n",100);
+    
+    printf("My height is %dcm \n",176);
+    
+    printf("I have older brother,he is %d years old \n",18);
+    
+    printf("Me:I have a qustion? \n");
+    
+    printf("Brother:What? \n");
+    
+    printf("Do you speak japanese? \n");
+    
+    return 0;
+}
